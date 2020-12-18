fbd9df3bec600e41d642f58bb40bc90baca0dc69
Marcin Ludwisiak <marcin.ludwisiak@unilodz.eu>
2020-12-18 19:11:43 +0100

    testing...

--- src/Game.java
+++ src/Game.java
@@ -10,9 +10,10 @@ public class Game {
 
     public Game() {
         this.player1 = new Player('1');
-        this.player1 = new Player('2');
+        this.player2 = new Player('2');
         this.board1 = new Board(player1, player2);
-        this.currentPlayer = new Player('1');
+        // current Player is Player1:
+        this.currentPlayer = this.player1;
     }
 
     public Game(Player player1, Player player2, Board board1, Player currentPlayer) {
@@ -94,38 +95,52 @@ public class Game {
 
         System.out.println("        => Choose the chessman to move: <= \n\n"
                                 + "Enter row number:" );
+//        int tmpR1 = 7;
+//        int tmpC1 = 2;
+//        int tmpR2 = 5;
+//        int tmpC2 = 2;
+//
+//        game1.getBoard1().setChessmanToMove(new Movement(tmpR1 - 1, tmpC1 - 1));
+//        game1.getBoard1().setNextMovement(new Movement(tmpR2 - 1, tmpC2 - 1));
+//        System.out.println(game1.getBoard1().checkMovement());
+
+
+        // odkomentuj:
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int _i = -1;
-        _i = Integer.parseInt(reader.readLine());
+
+        int row1 = -1;
+        row1 = Integer.parseInt(reader.readLine());
+
         System.out.println("Enter column name: ");
-        String inputColumn = reader.readLine();
+        String tmp1 = reader.readLine();
 
         // if entered chessman:
-        if (_i != -1) {
-            int _j = game1.convertColumn(inputColumn.charAt(0));
-            int iMinusOne = _i--;
-            // add 1 to i in order to use real world notation!
-            Chessman chessman = game1.getBoard1().getChessmen().get(iMinusOne).get(_j);
+        if (row1 != -1) {
+            int column1 = game1.convertColumn(tmp1.charAt(0));
+
+            // (row1 - 1) and (column1 - 1) in order to use real world notation!
+            Chessman chessman = game1.getBoard1().getChessmen().get(row1 - 1).get(column1 - 1);
 
             // if it's your chessman:
             if (chessman.getPlayer().getName() == game1.getCurrentPlayer().getName()) {
                 // set which chessman to move:
-                game1.getBoard1().setChessmanToMove(new Movement(iMinusOne, _j));
+                // =>  (row1 - 1) and (column1 - 1) in order to use real world notation!
+                game1.getBoard1().setChessmanToMove(new Movement(row1 - 1, column1 - 1));
 
                 // enter where to move it:
-                System.out.println("        => Enter where to move: <= \n"
+                System.out.println("        => Enter where to move: <= \n\n"
                                         + "Enter row number:" );
-                int _k =  Integer.parseInt(reader.readLine());
-
-                int kMinusOne = _k--;
+                int row2 =  Integer.parseInt(reader.readLine());
 
                 System.out.println("Enter column number:" );
-                String destinationColumn = reader.readLine();
+                String tmp2 = reader.readLine();
+
                 // extract the index:
-                int _l = game1.convertColumn(destinationColumn.charAt(0));
+                int column2 = game1.convertColumn(tmp2.charAt(0));
 
                 // set the next movement:
-                game1.getBoard1().setNextMovement(new Movement(kMinusOne, _l));
+                // =>  (row2 - 1) and (column2 - 1) in order to use real world notation!
+                game1.getBoard1().setNextMovement(new Movement(row2 - 1, column2 - 1));
 
                 // and check the movement of the chosen chessman:
                 System.out.println(game1.getBoard1().checkMovement());
